f8fc501799c050924d5e335d85d64ef892e0471a
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2024-10-07 08:21:27 +0530

    updated is-prime.js

--- Getting Started/is-prime.js
+++ Getting Started/is-prime.js
@@ -1,22 +1,12 @@
-// Function to check if a number is prime
 function isPrime(num) {
-  // Edge cases
-  if (num <= 1) return false; // 0 and 1 are not prime numbers
-  if (num <= 3) return true;  // 2 and 3 are prime numbers
+	if (num <= 1) return false;
+	if (num <= 3) return true;
 
-  // Check for divisibility by 2 or 3
-  if (num % 2 === 0 || num % 3 === 0) return false;
+	if (num % 2 === 0 || num % 3 === 0) return false;
 
-  // Check for divisibility from 5 to sqrt(num)
-  for (let i = 5; i * i <= num; i += 6) {
-    if (num % i === 0 || num % (i + 2) === 0) return false;
-  }
+	for (let i = 5; i * i <= num; i += 6) {
+		if (num % i === 0 || num % (i + 2) === 0) return false;
+	}
 
-  return true;
+	return true;
 }
-
-// Example usage
-console.log(isPrime(11)); // true
-console.log(isPrime(15)); // false
-console.log(isPrime(17)); // true
-// O(sqrt(n)) time complexity
