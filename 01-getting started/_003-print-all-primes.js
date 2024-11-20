225a9be88a651f8850a0e1eed8c49ef2c2ac0241
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2024-11-20 19:31:23 +0530

    feat: Updated _003

--- 01-getting started/_003-print-all-primes.js
+++ 01-getting started/_003-print-all-primes.js
@@ -1,20 +1,19 @@
 function allPrimes(n = 5) {
-  for(let i = 2; i <= n; i++) {
-    // check for each i
-    let isPrime = true;
-    let div = 2;
-    while(div * div <= i) {
-      const rem = i % div;
-      if(rem === 0) {
-        isPrime = false;
-        break;
-      }
-    }
-    if(isPrime) {
-      console.log(i + " is Prime")
-    } else { 
-      console.log(i + " is not Prime")
-    }
-  }
+	for (let i = 2; i <= n; i++) {
+		// check for each i
+		let isPrime = true;
+		let div = 2;
+		while (div * div <= i) {
+			const rem = i % div;
+			if (rem === 0) {
+				isPrime = false;
+				break;
+			}
+      div++;
+		}
+		if (isPrime) {
+			console.log(i + " is Prime");
+		}
+	}
 }
-allPrimes();
+allPrimes(10);
