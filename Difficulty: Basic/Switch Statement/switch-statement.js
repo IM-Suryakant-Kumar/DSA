ecf7895e39d675ad0669b0ab5f4ff7499b6ff550
Suryakant Kumar <66691162+IM-Suryakant-Kumar@users.noreply.github.com>
2025-03-18 16:34:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Switch Statement/switch-statement.js
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+// Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => { inputString += inputStdin; });
+
+process.stdin.on('end', _ => {
+    inputString =
+        inputString.trim().split('\n').map(string => { return string.trim(); });
+
+    main();
+});
+
+function readLine() { return inputString[currentLine++]; }
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for (; i < t; i++) {
+        let a = parseInt(readLine());
+        let obj = new Solution();
+        console.log(obj.utility(a));
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for javascript
+
+class Solution {
+    utility(number) {
+        // write your switch statement here
+        switch(number) {
+            case 1:
+                return "One";
+            case 2:
+                return "Two";
+            case 3:
+                return "Three";
+            case 4:
+                return "Four";
+            case 5:
+                return "Five";
+            case 6:
+                return "Six";
+            case 7:
+                return "Seven";
+            case 8:
+                return "Eight";
+            case 9:
+                return "Nine";
+            default:
+                return "Unknown";
+        }
+    }
+}
